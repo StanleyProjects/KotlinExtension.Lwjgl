ba98e8ef5c1d66e82a0b60c61c999715a7771e77
Stanley Wintergreen <kepocnhh@gmail.com>
2022-04-03 17:02:59 +0300

    init sample

---
+++ sample/build.gradle.kts
@@ -0,0 +1,20 @@
+repositories.mavenCentral()
+
+plugins {
+    id("application")
+    id("org.jetbrains.kotlin.jvm")
+}
+
+application {
+    mainClass.set("sp.service.sample.AppKt")
+}
+
+val jvmTarget = "1.8"
+
+tasks.getByName<JavaCompile>("compileJava") {
+    targetCompatibility = jvmTarget
+}
+
+tasks.getByName<org.jetbrains.kotlin.gradle.tasks.KotlinCompile>("compileKotlin") {
+    kotlinOptions.jvmTarget = jvmTarget
+}
