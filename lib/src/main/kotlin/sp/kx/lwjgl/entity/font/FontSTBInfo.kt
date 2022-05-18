9bafd4f5ad316995454d7c5ff036f40091fc3b0e
Stanley Wintergreen <kepocnhh@gmail.com>
2022-05-18 16:56:34 +0300

    stb...

--- lib/src/main/kotlin/sp/kx/lwjgl/entity/font/FontSTBInfo.kt
+++
@@ -1,9 +0,0 @@
-package sp.kx.lwjgl.entity.font
-
-import org.lwjgl.stb.STBTTPackedchar
-
-data class FontSTBInfo(
-    val textureId: Int,
-    val lineHeight: Float,
-    val buffer: STBTTPackedchar.Buffer
-)
