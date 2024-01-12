26655bc2c47f42a1d1c9f65bc0d1c05e2bf73765
Stanley Wintergreen <kepocnhh@gmail.com>
2024-01-12 19:52:58 +0300

    0.1.0

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # KotlinExtension.Lwjgl
-A few Lwjgl extensions.
+A few [Lwjgl](https://lwjgl.org/) extensions.
+
+---
+
+## Snapshot
+
+![version](https://img.shields.io/static/v1?label=version&message=0.1.0-SNAPSHOT&labelColor=212121&color=2962ff&style=flat)
--- lib/build.gradle.kts
+++ lib/build.gradle.kts
@@ -1,3 +1,26 @@
+import sp.gx.core.Badge
+import sp.gx.core.GitHub
+import sp.gx.core.Markdown
+import sp.gx.core.Maven
+import sp.gx.core.camelCase
+import sp.gx.core.check
+import sp.gx.core.colonCase
+import sp.gx.core.kebabCase
+import sp.gx.core.resolve
+import java.util.Locale
+
+version = "0.1.0"
+
+val maven = Maven.Artifact(
+    group = "com.github.kepocnhh",
+    id = rootProject.name,
+)
+
+val gh = GitHub.Repository(
+    owner = "StanleyProjects",
+    name = rootProject.name,
+)
+
 repositories {
     mavenCentral()
     maven("https://s01.oss.sonatype.org/content/repositories/snapshots")
@@ -24,5 +47,36 @@ dependencies {
     implementation(platform("$group:lwjgl-bom:${Version.lwjgl}"))
     LwjglUtil.modules.forEach { name ->
         implementation(group = group, name = name)
+    }
+}
+
+"snapshot".also { variant ->
+    val version = kebabCase(version.toString(), variant.uppercase(Locale.US))
+    // todo
+    task(camelCase("check", variant, "Readme")) {
+        doLast {
+            val badge = Markdown.image(
+                text = "version",
+                url = Badge.url(
+                    label = "version",
+                    message = version,
+                    color = "2962ff",
+                ),
+            )
+            val expected = setOf(
+                badge,
+                Markdown.link("Maven", Maven.Snapshot.url(maven, version)),
+                Markdown.link("Documentation", gh.pages().resolve("doc").resolve(version)),
+                "implementation(\"${colonCase(maven.group, maven.id, version)}\")",
+            )
+            val report = layout.buildDirectory.get()
+                .dir("reports/analysis/readme")
+                .file("index.html")
+                .asFile
+            rootDir.resolve("README.md").check(
+                expected = expected,
+                report = report,
+            )
+        }
     }
 }
