a3242196dbf9cfd06e7cb7d2e1464d70418b0aa0
Stanley Wintergreen <kepocnhh@gmail.com>
2024-08-24 17:53:59 +0300

    fix license

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,8 @@
+import sp.gx.core.asFile
+import sp.gx.core.buildDir
+import sp.gx.core.buildSrc
+import sp.gx.core.check
+
 buildscript {
     repositories.mavenCentral()
 
@@ -7,5 +12,18 @@ buildscript {
 }
 
 task<Delete>("clean") {
-    delete = setOf(buildDir, "buildSrc/build")
+    delete = setOf(buildDir(), buildSrc.buildDir())
+}
+
+task("checkLicense") {
+    doLast {
+        val author = "Stanley Wintergreen" // todo
+        file("LICENSE").check(
+            expected = emptySet(),
+            regexes = setOf("^Copyright 2\\d{3} $author${'$'}".toRegex()),
+            report = buildDir()
+                .dir("reports/analysis/license")
+                .asFile("index.html"),
+        )
+    }
 }
