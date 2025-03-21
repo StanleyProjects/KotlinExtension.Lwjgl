4dec66704557dbf54bd576d28e99480ce46c8503
Stanley Wintergreen <kepocnhh@gmail.com>
2025-03-21 21:26:20 +0300

    perspective

--- sample/src/main/kotlin/sp/service/sample/App.kt
+++ sample/src/main/kotlin/sp/service/sample/App.kt
@@ -3,12 +3,14 @@ package sp.service.sample
 import sp.kx.calculations.MutableSize
 import sp.kx.lwjgl.engine.Engine
 import sp.service.sample.logics.OrthoLogics
+import sp.service.sample.logics.PerspectiveLogics
 import sp.service.sample.logics.TestLogics
 
 fun main() {
 	Engine.run(
 //		title = "Test", supplier = ::TestLogics,
-		title = "Ortho", supplier = ::OrthoLogics,
+//		title = "Ortho", supplier = ::OrthoLogics,
+		title = "Perspective", supplier = ::PerspectiveLogics,
 		size = MutableSize(640.0, 480.0),
 		defaultFontName = "JetBrainsMono.ttf",
 	)
